8d3f687dc5d10f633b6d6c67924a4eedcac50732
diogo_gallina <diogogallina20@gmail.com>
2024-04-30 09:48:01 -0300

    feat: create address model

---
+++ src/main/java/br/com/fiap/eventmanager/models/Address.java
@@ -0,0 +1,26 @@
+package br.com.fiap.eventmanager.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "JV_CP2_ADDRESS")
+public class Address {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "address_id")
+    private Long id;
+
+    @Column(name = "street", nullable = false, length = 100)
+    private String street;
+
+    @Column(name = "city", nullable = false, length = 100)
+    private String city;
+
+    @Column(name = "postal_code", nullable = false, length = 8)
+    private String cep;
+
+    @Column(name = "address_number", nullable = false)
+    private Number number;
+
+}
