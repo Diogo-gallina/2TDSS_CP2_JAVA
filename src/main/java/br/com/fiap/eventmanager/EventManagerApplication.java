53b9e3acd930e02a819faa8f2d4e5c5bd7d8ba2d
diogo_gallina <diogogallina20@gmail.com>
2024-04-29 21:44:46 -0300

    chore: up code config

---
+++ src/main/java/br/com/fiap/eventmanager/EventManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.eventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagerApplication.class, args);
+	}
+
+}
